db448092a1765c7e0780e716952b8751f952b68c
Artem <artemkkharitonov@yandex.ru>
2020-01-26 17:04:52 +0300

    add slider in header

--- martaup/js/main.js
+++ martaup/js/main.js
@@ -1,5 +1,12 @@
 $(function(){
-
+    $('.top-head__slider').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
+        arrows: false,
+        vertical: true,
+        dotsClass: 'top-head__slider-inner-dots',
+    })
 
 
     
