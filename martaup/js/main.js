64da5475601b5ad1bb06a946da67698f39ecfd6d
Artem <artemkkharitonov@yandex.ru>
2020-01-29 19:17:32 +0300

    add adaptive menu

--- martaup/js/main.js
+++ martaup/js/main.js
@@ -6,8 +6,30 @@ $(function(){
         arrows: false,
         vertical: true,
         dotsClass: 'top-head__slider-inner-dots',
-    })
-
+    });
+    
+    // $('.nav__btn').on('click', function(){
+    //     $('.header__nav').slideToggle();
+    //     $(this).toggleClass('active');
+    // });
 
     
+});
+
+nav__btn.onclick = function() {
+    let menu = document.querySelector('.header__nav');
+    let header = document.querySelector('.header');
+    this.classList.toggle('active');
+    if (this.className == 'active') {
+        menu.style.display = 'block';
+    } else {
+        menu.style.display = 'none';
+    }
+}
+window.addEventListener('scroll', function(){
+    let menu = document.querySelector('.header__nav');
+    if (document.body.clientWidth < 690) {
+        menu.style.display = 'none';
+        nav__btn.className = '';
+    }
 });
