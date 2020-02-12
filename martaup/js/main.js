318c123ca0db753011d5d3ac66092721a9c059ce
Artem <artemkkharitonov@yandex.ru>
2020-02-12 12:36:55 +0300

    fixed js

--- martaup/js/main.js
+++ martaup/js/main.js
@@ -7,13 +7,6 @@ $(function(){
         vertical: true,
         dotsClass: 'top-head__slider-inner-dots',
     });
-    
-    // $('.nav__btn').on('click', function(){
-    //     $('.header__nav').slideToggle();
-    //     $(this).toggleClass('active');
-    // });
-
-    
 });
 
 //!!!!!!!!!!!!!!!!!!!MENU BUTTON!!!!!!!!!!!!!!!!!!!
@@ -61,81 +54,3 @@ function animationSvgReverse() {
     workAnim[1].setAttribute('transform', 'matrix(-0.878059 -0.495785 0.504134 -0.853958 406.954 480.478)');
 }
 setInterval(animationSvgReverse, 4000);
-
-
-
-
-
-
-
-
-// function animationSvg() {
-//     let featuresAnim = document.querySelectorAll('.features__image > svg > rect');
-//     let workAnim = document.querySelectorAll('.work__image > svg > rect');
-//     if (featuresAnim[1].getAttribute('transform', 'matrix(0.875951 -0.482401 0.517808 0.855497 -339 217.243)') ||
-//     featuresAnim[2].getAttribute('transform', 'matrix(0.875951 -0.482401 0.517808 0.855497 -267.724 245.926)') ||
-//     featuresAnim[0].getAttribute('transform', 'matrix(0.875951 -0.482401 0.517808 0.855497 -294.06 348.641)') ||
-//     workAnim[0].getAttribute('transform', 'matrix(-0.878059 -0.495785 0.504134 -0.853958 489.815 399.813)') ||
-//     workAnim[2].getAttribute('transform', 'matrix(-0.878059 -0.495785 0.504134 -0.853958 398.337 413.07)') ||
-//     workAnim[1].getAttribute('transform', 'matrix(-0.878059 -0.495785 0.504134 -0.853958 406.954 480.478)')) {
-//         featuresAnim[1].setAttribute('transform', 'matrix(0.875951 -0.482401 0.517808 0.855497 -400 247.243)');
-//         featuresAnim[2].setAttribute('transform', 'matrix(0.875951 -0.482401 0.517808 0.855497 -400 318.926)');
-//         featuresAnim[0].setAttribute('transform', 'matrix(0.875951 -0.482401 0.517808 0.855497 -400 398.641)');
-//         workAnim[0].setAttribute('transform', 'matrix(-0.878059 -0.495785 0.504134 -0.853958 600 459.813)');
-//         workAnim[2].setAttribute('transform', 'matrix(-0.878059 -0.495785 0.504134 -0.853958 600 528.07)');
-//         workAnim[1].setAttribute('transform', 'matrix(-0.878059 -0.495785 0.504134 -0.853958 600 590.478)');
-//     }
-//     console.log(featuresAnim[1].getAttribute('transform'));
-// }
-// setInterval(animationSvg, 2000);
-
-// function animationSvgReverse() {
-//     let featuresAnim = document.querySelectorAll('.features__image > svg > rect');
-//     let workAnim = document.querySelectorAll('.work__image > svg > rect');
-//     if (featuresAnim[1].getAttribute('transform', 'matrix(0.875951 -0.482401 0.517808 0.855497 -400 247.243)') ||
-//         featuresAnim[2].getAttribute('transform', 'matrix(0.875951 -0.482401 0.517808 0.855497 -400 318.926)') ||
-//         featuresAnim[0].getAttribute('transform', 'matrix(0.875951 -0.482401 0.517808 0.855497 -400 398.641)') ||
-//         workAnim[0].getAttribute('transform', 'matrix(-0.878059 -0.495785 0.504134 -0.853958 600 459.813)') ||
-//         workAnim[2].getAttribute('transform', 'matrix(-0.878059 -0.495785 0.504134 -0.853958 600 528.07)') ||
-//         workAnim[1].getAttribute('transform', 'matrix(-0.878059 -0.495785 0.504134 -0.853958 600 590.478)')) {
-//             featuresAnim[1].setAttribute('transform', 'matrix(0.875951 -0.482401 0.517808 0.855497 -339 217.243)');
-//             featuresAnim[2].setAttribute('transform', 'matrix(0.875951 -0.482401 0.517808 0.855497 -267.724 245.926)');
-//             featuresAnim[0].setAttribute('transform', 'matrix(0.875951 -0.482401 0.517808 0.855497 -294.06 348.641)');
-//             workAnim[0].setAttribute('transform', 'matrix(-0.878059 -0.495785 0.504134 -0.853958 489.815 399.813)');
-//             workAnim[2].setAttribute('transform', 'matrix(-0.878059 -0.495785 0.504134 -0.853958 398.337 413.07)');
-//             workAnim[1].setAttribute('transform', 'matrix(-0.878059 -0.495785 0.504134 -0.853958 406.954 480.478)');
-//     }
-//     console.log(featuresAnim[1].getAttribute('transform'));
-// }
-// setInterval(animationSvgReverse, 4000);
-
-
-
-
-
-
-
-
-
-
-// let animationSvg = setInterval(function animationStart() {
-//     let featuresAnim = document.querySelectorAll('.features__image > svg > rect');
-//     let workAnim = document.querySelectorAll('.work__image > svg > rect');
-//     featuresAnim[1].setAttribute('transform', 'matrix(0.875951 -0.482401 0.517808 0.855497 -400 247.243)');
-//     featuresAnim[2].setAttribute('transform', 'matrix(0.875951 -0.482401 0.517808 0.855497 -400 318.926)');
-//     featuresAnim[0].setAttribute('transform', 'matrix(0.875951 -0.482401 0.517808 0.855497 -400 398.641)');
-//     workAnim[0].setAttribute('transform', 'matrix(-0.878059 -0.495785 0.504134 -0.853958 600 459.813)');
-//     workAnim[2].setAttribute('transform', 'matrix(-0.878059 -0.495785 0.504134 -0.853958 600 528.07)');
-//     workAnim[1].setAttribute('transform', 'matrix(-0.878059 -0.495785 0.504134 -0.853958 600 590.478)');
-//     animationSvg = setInterval(function animationEnd() {
-//         let featuresAnim = document.querySelectorAll('.features__image > svg > rect');
-//         let workAnim = document.querySelectorAll('.work__image > svg > rect');
-//         featuresAnim[1].setAttribute('transform', 'matrix(0.875951 -0.482401 0.517808 0.855497 -339 217.243)');
-//         featuresAnim[2].setAttribute('transform', 'matrix(0.875951 -0.482401 0.517808 0.855497 -267.724 245.926)');
-//         featuresAnim[0].setAttribute('transform', 'matrix(0.875951 -0.482401 0.517808 0.855497 -294.06 348.641)');
-//         workAnim[0].setAttribute('transform', 'matrix(-0.878059 -0.495785 0.504134 -0.853958 489.815 399.813)');
-//         workAnim[2].setAttribute('transform', 'matrix(-0.878059 -0.495785 0.504134 -0.853958 398.337 413.07)');
-//         workAnim[1].setAttribute('transform', 'matrix(-0.878059 -0.495785 0.504134 -0.853958 406.954 480.478)');
-//     }, 2000);
-// }, 2000);
-// console.log(document.querySelector('.features__image > svg > rect').getAttribute('transform'));
