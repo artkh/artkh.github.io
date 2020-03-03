6172729d4e6b895a06ff230b49722836c6f9da21
Artem <artemkkharitonov@yandex.ru>
2020-03-03 16:03:04 +0300

    fix and add new js code

--- ozon/js/main.js
+++ ozon/js/main.js
@@ -2,7 +2,7 @@
 let price = document.querySelectorAll('.price');
 function spacePrice() {
     for (let i = 0; i < price.length; i++) {
-        let p = price[i].innerHTML.replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 ');
+        let p = price[i].innerHTML.replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 '); //пробел после каждого третьего числа
         price[i].innerHTML = p;
     }
 }
@@ -11,27 +11,43 @@ spacePrice();
 
 // Кнопка добавления в корзину
 let buttonBasket = document.querySelectorAll('.basket-button');
+let clearBasket = document.querySelector('.clear-basket'); //кнопка очистки корзины
 for (let i = 0; i < buttonBasket.length; i++) {
     buttonBasket[i].addEventListener('click', function () {
-        localStorage.setItem(i, 'inBasket');
+        // localStorage.setItem(i, 'inBasket');
+        localStorage.setItem(this.dataset.idBasket, 'inBasket');
         this.classList.add('in-basket');
         this.innerHTML = 'В корзине';
         let inBasket = document.querySelectorAll('.in-basket');
         basket.querySelector('span').innerHTML = inBasket.length; // Пополнение корзины в хидере
+        clearBasket.style.display = 'block';
     });
 }
 
 window.addEventListener('DOMContentLoaded', function() {
     for (let i = 0; i < localStorage.length; i++) {
         let key = localStorage.key(i);
-        buttonBasket[key].classList.add('in-basket');
-        buttonBasket[key].innerHTML = 'В корзине';
-        let inBasket = document.querySelectorAll('.in-basket');
-        basket.querySelector('span').innerHTML = inBasket.length;
+        for (let i = 0; i < buttonBasket.length; i++) {
+            if (buttonBasket[i].dataset.idBasket == key) {
+                buttonBasket[i].classList.add('in-basket');
+                buttonBasket[i].innerHTML = 'В корзине';
+                let inBasket = document.querySelectorAll('.in-basket');
+                basket.querySelector('span').innerHTML = inBasket.length;
+                clearBasket.style.display = 'block';
+            }
+        }
     }
 
+    // for (let i = 0; i < localStorage.length; i++) {
+    //     let key = localStorage.key(i);
+    //     buttonBasket[key].classList.add('in-basket');
+    //     buttonBasket[key].innerHTML = 'В корзине';
+    //     let inBasket = document.querySelectorAll('.in-basket');
+    //     basket.querySelector('span').innerHTML = inBasket.length;
+    // }
+
     //очистка корзины
-    basket.addEventListener('click', function() {
+    clearBasket.addEventListener('click', function() {
         localStorage.clear();
         location.reload();
     });
@@ -64,13 +80,32 @@ function calcDiscount() {
             let totalCalc = Math.floor((calcPercent * 100) / 100); //убираю знаки после запятой
             discountIn[i].innerHTML = totalCalc + '&thinsp;₽'; // сумма с учетом скидки вставлена
             // if(totalCalc > 9999) {
-            //     let p = discountIn[i].innerHTML.replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 '); // возвращаю пробелы в цене
+            //     let p = discountIn[i].innerHTML.replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 '); // возвращаю пробелы в цене после 9999
             //     discountIn[i].innerHTML = p;
             // }
-            let p = discountIn[i].innerHTML.replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 '); // возвращаю пробелы в цене
+            let p = discountIn[i].innerHTML.replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 '); // возвращаю пробелы в цене после каждого третьего числа
             discountIn[i].innerHTML = p;
         }
     }
 }
 calcDiscount();
 // расчёт скидки конец
+
+// Обрезка текста по кол-ву символов
+function textCropping() {
+    let elem = document.querySelectorAll('.catalog__item-title');
+    let text = elem.innerHTML;
+    let size = 72;
+    let croppingTo = 67;
+
+    for (let i = 0; i < elem.length; i++) {
+        if (elem[i].innerHTML.length > size) {
+            text = elem[i].innerHTML.substr(0, croppingTo);
+            elem[i].innerHTML = text + '...';
+        } else {
+            text = elem[i].innerHTML;
+        }
+    }
+}
+textCropping();
+// Обрезка конец
