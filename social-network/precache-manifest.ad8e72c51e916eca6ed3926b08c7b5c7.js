96b942042f7b9dd93dfce761485b602d3aef0988
Artkh <artemkkharitonov@yandex.ru>
2020-05-06 16:31:23 +0300

    add project

---
+++ social-network/precache-manifest.ad8e72c51e916eca6ed3926b08c7b5c7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79af774848f0b2f72cd662509881e8ca",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "c42fced658ccc58aa15b",
+    "url": "/social-network/static/css/4.486937fc.chunk.css"
+  },
+  {
+    "revision": "2472c6668bca3de7de1f",
+    "url": "/social-network/static/css/5.eafaa66b.chunk.css"
+  },
+  {
+    "revision": "010822e64458e407e034",
+    "url": "/social-network/static/css/6.589a4303.chunk.css"
+  },
+  {
+    "revision": "a5a435aa4ce9f3f19519",
+    "url": "/social-network/static/css/7.b36c350c.chunk.css"
+  },
+  {
+    "revision": "4af54e2f69a0f58dbbe9",
+    "url": "/social-network/static/css/main.49c24391.chunk.css"
+  },
+  {
+    "revision": "55a9e42ebab1a328c42f",
+    "url": "/social-network/static/js/0.4175adb5.chunk.js"
+  },
+  {
+    "revision": "bd1621824b1cbdc0ebd0",
+    "url": "/social-network/static/js/3.a4f64405.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/social-network/static/js/3.a4f64405.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c42fced658ccc58aa15b",
+    "url": "/social-network/static/js/4.140f0c8f.chunk.js"
+  },
+  {
+    "revision": "2472c6668bca3de7de1f",
+    "url": "/social-network/static/js/5.bda4a186.chunk.js"
+  },
+  {
+    "revision": "010822e64458e407e034",
+    "url": "/social-network/static/js/6.b122eb19.chunk.js"
+  },
+  {
+    "revision": "a5a435aa4ce9f3f19519",
+    "url": "/social-network/static/js/7.3a8351e3.chunk.js"
+  },
+  {
+    "revision": "4af54e2f69a0f58dbbe9",
+    "url": "/social-network/static/js/main.cfe3e34d.chunk.js"
+  },
+  {
+    "revision": "0c48b24d3c3eac5488e7",
+    "url": "/social-network/static/js/runtime-main.aeb0722e.js"
+  }
+]);
