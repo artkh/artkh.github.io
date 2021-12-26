18cbd7436853013a1760cec5808104f3e7eadc3c
Artem <57627585+artkh@users.noreply.github.com>
2021-12-26 16:48:12 +0300

    add new project - smclinic

---
+++ smclinic/js/main.js
@@ -0,0 +1,57 @@
+document.addEventListener('DOMContentLoaded', function() {
+	//work banner
+	let scrollBaner = scroll_banner;
+
+  window.addEventListener('scroll', function(){
+
+    let bottomPointBannerOnPage = document.body.querySelector('.banner').getBoundingClientRect().bottom;
+    
+		if(bottomPointBannerOnPage <= 0) {
+			scrollBaner.classList.add('active');
+			scrollBaner.classList.add('animate__slideInUp');
+		} else {
+			scrollBaner.classList.remove('active');
+			scrollBaner.classList.remove('animate__slideInUp');
+		}
+
+  });
+	//work banner END
+
+	//work nav
+	let navLink = nav.querySelectorAll('.category');
+
+	let showDropDownMenu = function () {
+		for (let i = 0; i < navLink.length; i++) {
+			navLink[i].addEventListener('mouseenter', function() {
+				if(this.querySelector('.dropdown__menu')) {
+					this.querySelector('.dropdown__menu').classList.add('active');
+					this.querySelector('.first_link').style.borderRadius = `10px 10px 0 0`;
+				}
+			})
+
+			navLink[i].addEventListener('mouseleave', function() {
+				if(this.querySelector('.dropdown__menu')) {
+					this.querySelector('.dropdown__menu').classList.remove('active');
+					this.querySelector('.first_link').style = '';
+				}
+			})
+		}
+
+		//open and close navMenu in mobile devices
+		let buttonOpenNav = document.querySelector('.open_close_nav');
+		buttonOpenNav.addEventListener('click', function() {
+			buttonOpenNav.classList.toggle('close');
+			if(buttonOpenNav.classList.contains('close')) {
+				document.body.classList.add('overflow-hidden');
+			} else {
+				document.body.classList.remove('overflow-hidden');
+			}
+		})
+	}
+
+	if(navLink) {
+		showDropDownMenu();
+	}
+	//work nav END
+
+});
