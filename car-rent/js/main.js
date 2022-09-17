a431b2d9dae5cd41a41a5ee46b2480a1d335d8e3
artemKh <temanike@yandex.ru>
2022-09-17 15:01:34 +0300

    add time limits for form search cars

--- car-rent/js/main.js
+++ car-rent/js/main.js
@@ -28,13 +28,55 @@ document.addEventListener('DOMContentLoaded', function () {
   }
   //end
 
-  // смена аренды
+  // поиск авто
   let searchCars = document.querySelector('.search_cars');
-  let searchCarsSwap = document.querySelector('.search_cars').querySelector('.swap');
 
-  searchCarsSwap.addEventListener('click', function() {
-    searchCars.querySelectorAll('.search_cars__wrapper_item')[0].classList.toggle('swap_active');
-  })
+  if(searchCars) {
+    
+    let searchCarsSwap = document.querySelector('.search_cars').querySelector('.swap');
+    searchCarsSwap.addEventListener('click', function() {
+      searchCars.querySelectorAll('.search_cars__wrapper_item')[0].classList.toggle('swap_active');
+    });
+
+    // настройка лимитов выбора времени
+
+    let searchCarsDate = searchCars.querySelectorAll('input[type="date"]');
+    let searchCarsTime = searchCars.querySelectorAll('input[type="time"]');
+    let nowDate = new Date();
+    let nowDateYear = nowDate.getFullYear();
+    let nowDateMonth = nowDate.getMonth();
+
+    ++nowDateMonth;
+
+    if(nowDateMonth < 10) {
+      nowDateMonth = `0${nowDateMonth}`;
+    }
+
+    let nowDateNum = nowDate.getDate();
+
+    if(nowDateNum < 10) {
+      nowDateNum = `0${nowDateNum}`;
+    }
+    
+    let nowDateTimeHours = nowDate.getHours();
+    let nowDateTimeMinutes = nowDate.getMinutes();
+
+    if(nowDateTimeHours < 10) {
+      nowDateTimeHours = `0${nowDateTimeHours}`;
+    }
+    
+    if(nowDateTimeMinutes < 10) {
+      nowDateTimeMinutes = `0${nowDateTimeMinutes}`;
+    }
+
+    for(let i = 0; i < searchCarsDate.length; i++) {
+      searchCarsDate[i].min = `${nowDateYear}-${nowDateMonth}-${nowDateNum}`;
+      searchCarsDate[i].value = `${nowDateYear}-${nowDateMonth}-${nowDateNum}`;
+
+      searchCarsTime[i].value = `${nowDateTimeHours}:${nowDateTimeMinutes}`;
+    }
+  }
+
   // end
 
   // добавленние в избранное
