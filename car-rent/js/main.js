f7e634d449e490419fe28b037bf4f04b9364580c
artemKh <temanike@yandex.ru>
2022-10-10 01:47:41 +0400

    working button add fin favorits

--- car-rent/js/main.js
+++ car-rent/js/main.js
@@ -10,14 +10,14 @@ document.addEventListener('DOMContentLoaded', function () {
     searchFilter.addEventListener('click', function () {
       searchFilterList.classList.toggle('active');
 
-      if(searchFilterList.classList.contains('active')) {
-        document.addEventListener('scroll', function() {
+      if (searchFilterList.classList.contains('active')) {
+        document.addEventListener('scroll', function () {
           searchFilterList.classList.remove('active');
         });
       }
     })
 
-    searchString.addEventListener('focus', function() {
+    searchString.addEventListener('focus', function () {
       searchFilterList.classList.remove('active');
     })
   }
@@ -26,10 +26,10 @@ document.addEventListener('DOMContentLoaded', function () {
   // поиск авто
   let searchCars = document.querySelector('.search_cars');
 
-  if(searchCars) {
-    
+  if (searchCars) {
+
     let searchCarsSwap = document.querySelector('.search_cars').querySelector('.swap');
-    searchCarsSwap.addEventListener('click', function() {
+    searchCarsSwap.addEventListener('click', function () {
       searchCars.querySelectorAll('.search_cars__wrapper_item')[0].classList.toggle('swap_active');
     });
 
@@ -43,28 +43,28 @@ document.addEventListener('DOMContentLoaded', function () {
 
     ++nowDateMonth;
 
-    if(nowDateMonth < 10) {
+    if (nowDateMonth < 10) {
       nowDateMonth = `0${nowDateMonth}`;
     }
 
     let nowDateNum = nowDate.getDate();
 
-    if(nowDateNum < 10) {
+    if (nowDateNum < 10) {
       nowDateNum = `0${nowDateNum}`;
     }
-    
+
     let nowDateTimeHours = nowDate.getHours();
     let nowDateTimeMinutes = nowDate.getMinutes();
 
-    if(nowDateTimeHours < 10) {
+    if (nowDateTimeHours < 10) {
       nowDateTimeHours = `0${nowDateTimeHours}`;
     }
-    
-    if(nowDateTimeMinutes < 10) {
+
+    if (nowDateTimeMinutes < 10) {
       nowDateTimeMinutes = `0${nowDateTimeMinutes}`;
     }
 
-    for(let i = 0; i < searchCarsDate.length; i++) {
+    for (let i = 0; i < searchCarsDate.length; i++) {
       searchCarsDate[i].min = `${nowDateYear}-${nowDateMonth}-${nowDateNum}`;
       searchCarsDate[i].value = `${nowDateYear}-${nowDateMonth}-${nowDateNum}`;
 
@@ -74,64 +74,29 @@ document.addEventListener('DOMContentLoaded', function () {
 
   // end
 
-  // добавленние в избранное
-  function allInFavoritesButton(section) {
-    let inFavoritesButton = section.querySelectorAll('.top_info__favorites');
-    for(let i = 0; i < inFavoritesButton.length; i++) {
-      inFavoritesButton[i].addEventListener('click', function() {
-        this.classList.toggle('in_favorites');
-      })
-    }
-  }
-  //end
-
   // вставка информации о авто из json
   function openCarsInform(url, section) {
     fetch(url).then(response => response.json())
-    .then(result => setInformCars(result, section));
+      .then(result => setInformCars(result, section));
   }
 
-  function setInformCars (result, section) {
+  function setInformCars(result, section) {
     let listCars = result.listCars;
 
-    // for(let i = 0; i < listCars.length; i++) {
-    //   elem[i].querySelector('.top_info__name').innerHTML = listCars[i].name;
-    //   elem[i].querySelector('.top_info__class').innerHTML = listCars[i].class;
-
-    //   if(listCars[i].inFavorites) {
-    //     elem[i].querySelector('.top_info__favorites').classList.add('in_favorites')
-    //   }
-
-    //   for (let j = 0; j < elem[i].querySelectorAll('a').length; j++) {
-    //     elem[i].querySelectorAll('a')[j].href = listCars[i].link;
-    //   }
-
-    //   elem[i].querySelector('.image').querySelector('img').src = listCars[i].image;
-    //   elem[i].querySelector('.spec').querySelector('.fuel').innerHTML = `${listCars[i].fuel}l`;
-    //   elem[i].querySelector('.spec').querySelector('.gearbox__manual').innerHTML = listCars[i].gearbox;
-    //   elem[i].querySelector('.spec').querySelector('.people').innerHTML = `${listCars[i].people} People`;
-    //   elem[i].querySelector('.rent').querySelector('.rent__price_finalcost').innerHTML = `${listCars[i].priceFinal}.00/`;
-    //   elem[i].querySelector('.rent').querySelector('.rent__price_finalcost').innerHTML = `${listCars[i].priceFinal}.00/`;
-    //   elem[i].querySelector('.rent').querySelector('.rent__price_time').innerHTML = listCars[i].rentTime;
-
-    //   if(listCars[i].priceDiscount) {
-    //     let div = document.createElement('div');
-    //     div.className = "rent__price_discount";
-    //     div.innerHTML = `$${listCars[i].priceDiscount}.00`;
-    //     elem[i].querySelector('.rent').querySelector('.rent__price').append(div);
-    //   }
-    // }
-
-    if(section == mostPopular) {
-      for(let i = 0 ; i <= 3; i++) {
+    if (section == mostPopular) {
+      for (let i = 0; i <= 3; i++) {
         createCarItemHTML(section, listCars[i].name, listCars[i].class, listCars[i].inFavorites, listCars[i].image, listCars[i].fuel, listCars[i].gearbox, listCars[i].people, listCars[i].priceFinal, listCars[i].rentTime, listCars[i].priceDiscount, listCars[i].link);
       }
+      
+      allInFavoritesButton(section, 0);
     }
 
-    if(section == recomendedCars) {
-      for(let i = 0 ; i <= 7; i++) {
+    if (section == recomendedCars) {
+      for (let i = 0; i <= 7; i++) {
         createCarItemHTML(section, listCars[i].name, listCars[i].class, listCars[i].inFavorites, listCars[i].image, listCars[i].fuel, listCars[i].gearbox, listCars[i].people, listCars[i].priceFinal, listCars[i].rentTime, listCars[i].priceDiscount, listCars[i].link);
       }
+
+      allInFavoritesButton(section, 0);
 
       //открытие остальных авто
       const showMoreButton = section.querySelector('.more-car').querySelector('.primary_button');
@@ -141,19 +106,21 @@ document.addEventListener('DOMContentLoaded', function () {
       let elemAmount = section.querySelector('.category__items').querySelectorAll('.item').length;
       elemAmount >= listCars.length ? showMoreButton.style.display = 'none' : null;
 
-      showMoreButton.addEventListener('click', function() {
+      showMoreButton.addEventListener('click', function () {
         let notLoadCarsNum = listCars.length - elemAmount;
         let maxOpen = elemAmount + 8;
+        let currentlengthItems = section.querySelector('.category__items').querySelectorAll('.item').length;
 
-        if(notLoadCarsNum >= 1) {
-          for(let i = elemAmount; i < maxOpen; i++) {
+        if (notLoadCarsNum >= 1) {
+          for (let i = elemAmount; i < maxOpen; i++) {
             createCarItemHTML(section, listCars[i].name, listCars[i].class, listCars[i].inFavorites, listCars[i].image, listCars[i].fuel, listCars[i].gearbox, listCars[i].people, listCars[i].priceFinal, listCars[i].rentTime, listCars[i].priceDiscount, listCars[i].link);
             elemAmount++;
-            if(elemAmount == listCars.length) {
+            if (elemAmount == listCars.length) {
               showMoreButton.style.display = 'none';
               break;
             }
           }
+          allInFavoritesButton(section, currentlengthItems);
         }
       })
 
@@ -161,25 +128,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
   }
 
-  // function Car(name, classCar, inFavorites, image, fuel, gearbox, people, priceFinal, rentTime, priceDiscount, link) {
-  //   this.name = name;
-  //   this.class = classCar;
-  //   this.inFavorites = inFavorites;
-  //   this.image = image;
-  //   this.fuel = fuel;
-  //   this.gearbox = gearbox;
-  //   this.people = people;
-  //   this.priceFinal = priceFinal;
-  //   this.rentTime = rentTime;
-  //   this.priceDiscount = priceDiscount;
-  //   this.link = link;
-  // }
+  function createCarItemHTML(section, name, classCar, inFavorites, image, fuel, gearbox, people, priceFinal, rentTime, priceDiscount, link) {
+    let item = document.createElement('div');
 
-  function createCarItemHTML (section, name, classCar, inFavorites, image, fuel, gearbox, people, priceFinal, rentTime, priceDiscount, link) {
-    let div = document.createElement('div');
-
-    div.classList.add('item');
-    div.insertAdjacentHTML('afterbegin', `
+    item.classList.add('item');
+    item.insertAdjacentHTML('afterbegin', `
             <div class="inner">
 
               <div class="top_info">
@@ -202,7 +155,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 <div class="rent__price">
                   <span class="rent__price_finalcost">$${priceFinal}.00/</span>
                   <span class="rent__price_time">${rentTime}</span>
-                  <span class="rent__price_discount">$${priceDiscount}.00</span>
+                  <span class="rent__price_discount">${priceDiscount ? `$${priceDiscount}.00` : ''}</span>
                 </div>
 
                 <a href="${link}" class="rent__link primary_button primary_button__l">Rent Now</a>
@@ -211,15 +164,27 @@ document.addEventListener('DOMContentLoaded', function () {
             </div>`)
 
     let categoryItems = section.querySelector('.category__items');
-    categoryItems.append(div);
+    categoryItems.append(item);
   }
 
-  if(mostPopular) {
+  if (mostPopular) {
     openCarsInform('https://artkh.github.io/car-rent/json/popular-cars.json', mostPopular);
   }
 
-  if(recomendedCars) {
+  if (recomendedCars) {
     openCarsInform('https://artkh.github.io/car-rent/json/all-cars.json', recomendedCars);
+  }
+  //end
+
+  // избранное
+  function allInFavoritesButton(section, start) {
+    let inFavoritesButton = section.querySelectorAll('.top_info__favorites');
+
+    for (let i = start; i < inFavoritesButton.length; i++) {
+      inFavoritesButton[i].addEventListener('click', function () {
+        this.classList.toggle('in_favorites');
+      })
+    }
   }
   //end
 })
